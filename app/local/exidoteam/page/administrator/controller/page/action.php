2ce31ad091db5b2665281fc5344fdd7fed3cf357
Alexander Sharapov <Alex@alexs-macbook.local>
2013-08-27 00:06:58 +0700

    no message

---
+++ app/local/exidoteam/page/administrator/controller/page/action.php
@@ -0,0 +1,148 @@
+<?php defined('SYSPATH') or die('No direct script access allowed.');
+
+/*******************************************************************************
+ * ExidoEngine Web-sites manager
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License (3.0)
+ * that is bundled with this package in the file license_en.txt
+ * It is also available through the world-wide-web at this URL:
+ * http://www.exidoengine.com/license/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@exidoengine.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade ExidoEngine to newer
+ * versions in the future. If you wish to customize ExidoEngine for your
+ * needs please refer to http://www.exidoengine.com for more information.
+ *
+ * @license   http://www.exidoengine.com/license/gpl-3.0.html (GNU General Public License v3)
+ * @author    ExidoTeam
+ * @copyright Copyright (c) 2009 - 2013, ExidoEngine Solutions
+ * @link      http://www.exidoengine.com/
+ * @since     Version 1.0
+ * @filesource
+ *******************************************************************************/
+
+/**
+ * Administrator page controller class.
+ * @package    core
+ * @copyright  Sharapov A.
+ * @created    10/11/2012
+ * @version    1.0
+ */
+class Administrator_Controller_Page_Action extends Controller_Administrator_Abstract
+{
+  public $db_page;
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+    parent::__construct();
+    Helper::load('table', 'date', 'eav', 'form');
+    // Load EAV model
+    $this->db_page = $this->model('Model_Eav', 'page');
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Pages index page
+   * @return void
+   */
+  public function index()
+  {
+    $this->view->item_list = $this->db_page->getEntities();
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Pages create page
+   * @return void
+   */
+  public function create()
+  {
+    // Save when posting
+    if($this->input->checkPost()) {
+      $post = $this->input->post();
+      if($this->db_page->addEntity($post)) {
+        $this->session->set('action_success', __('Page has been successfully created.'));
+      } else {
+        // Error when saving
+        $this->session->set('action_error',
+                              sprintf(__('There is an error while creating a page. Details: %s'), $this->db_page->getErrorString())
+        );
+      }
+      uriSiteRedirect('page');
+    }
+    $this->view->attribute_form = $this->db_page->getAttributeSet('default');
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Pages edit page
+   * @param int $entity_id
+   * @return void
+   * @throws Exception_Exido
+   */
+  public function edit($entity_id = null)
+  {
+    // Get page data
+    if( ! $this->view->attribute_form = $this->db_page->getEntityById($entity_id)) {
+      throw new Exception_Exido(__('Page not found'), array(), 404);
+    };
+    // Save when posting
+    if($this->input->checkPost()) {
+      $post = $this->input->post();
+      if($this->db_page->editAttributeValues($entity_id, $post)) {
+        // Errors
+        $this->session->set('action_success', __('Page has been successfully saved.'));
+      } else {
+        // Error when saving
+        $this->session->set('action_error',
+                              sprintf(__('There is an error while saving a page. Details: %s'), $this->db_page->getErrorString())
+        );
+      }
+      uriSiteRedirect('page');
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Pages edit page
+   * @param int $entity_id
+   * @return void
+   * @throws Exception_Exido
+   */
+  public function remove($entity_id = null)
+  {
+    $this->disableViews();
+    // Get page data
+    if( ! $this->view->attribute_form = $this->db_page->getEntityById($entity_id)) {
+      throw new Exception_Exido(__('Page not found'), array(), 404);
+    };
+
+    if($this->db_page->removeEntity($entity_id)) {
+      // Errors
+      $this->session->set('action_success', __('Page has been successfully removed.'));
+    } else {
+      // Error when removing
+      $this->session->set('action_error',
+                          sprintf(__('There is an error while removing a page. Details: %s'), $this->db_page->getErrorString())
+      );
+    }
+    uriSiteRedirect('page');
+  }
+}
+
+?>
---
+++ app/local/exidoteam/page/administrator/template/default/action/page/index.php
@@ -0,0 +1,69 @@
+<?php defined('SYSPATH') or die('No direct script access allowed.');
+
+/*******************************************************************************
+ * ExidoEngine Web-sites manager
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License (3.0)
+ * that is bundled with this package in the file license_en.txt
+ * It is also available through the world-wide-web at this URL:
+ * http://www.exidoengine.com/license/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@exidoengine.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade ExidoEngine to newer
+ * versions in the future. If you wish to customize ExidoEngine for your
+ * needs please refer to http://www.exidoengine.com for more information.
+ *
+ * @license   http://www.exidoengine.com/license/gpl-3.0.html (GNU General Public License v3)
+ * @author    ExidoTeam
+ * @copyright Copyright (c) 2009 - 2013, ExidoEngine Solutions
+ * @link      http://www.exidoengine.com/
+ * @since     Version 1.0
+ * @filesource
+ *******************************************************************************/
+
+// List actions menu
+$view->action_menu = array(
+  '/page/action/create' => __('Create a new page')
+);
+// Include menu code
+$view->getView('layout/inc.list-action-menu-panel');
+
+$helper->heading(__('Static pages'));
+
+if($view->item_list) {
+  print tableOpen('-i-table -i-table-striped');
+  print tableHead(array(
+                    '',
+                    __('ID'),
+                    __('Page title'),
+                    __('Owner'),
+                    __('Group'),
+                    __('Added at'),
+                    __('Status'),
+                    __('Actions')
+                  ));
+  foreach($view->item_list as $item) {
+    print '<tr>'
+         .'<td>'.formCheckbox('item[]', $item->entity_id, false, 'class="item-list-checkbox"').'</td>'
+         .'<td>'.$item->entity_id.'</td>'
+         .'<td>'.eavFetchValue('title', $item->attributes).'</td>'
+         .'<td>'.eavFetchValue('owner_name', $item->attributes).'</td>'
+         .'<td>'.eavFetchValue('group_name', $item->attributes).'</td>'
+         .'<td>'.dateConvertSQL2Human(eavFetchValue('created_at', $item->attributes), Exido::config('global.date.format_long')).'</td>'
+         .'<td>'.eavFetchValue('is_enabled', $item->attributes, 'htmlStatus').'</td>'
+         .'<td>';
+    $helper->a('page/action/edit/'.$item->entity_id, __('Edit'));
+    $helper->a('page/action/remove/'.$item->entity_id, __('Remove'), 'remove');
+    print '</td></tr>';
+  }
+  print tableClose();
+} else {
+  $helper->notifier(__('No pages created'));
+}
+?>
